d06f0d8f4154010d91e774478148e40e0165ad75
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-02-05 23:54:45 +0530

    second commit

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
 let username:string = "Ashwani";
 console.log(username)
 
+let age: number = 20;
+
+function checkAge(userAge: number) {
+  if (userAge >= 18) {
+    console.log("You are eligible");
+  } else {
+    console.log("You are not eligible");
+  }
+}
+
+console.log(checkAge(age));
