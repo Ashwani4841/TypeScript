3cad48b1a83182cb59a823032e80d5c0b37b0db0
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-02-24 23:13:05 +0530

    ts example

--- index.ts
+++ index.ts
@@ -170,3 +170,9 @@ type Props = {
   title: string;
 };
 
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(10, 20)); // ✅ 30
+// console.log(add("10", 20)); ❌ Error
