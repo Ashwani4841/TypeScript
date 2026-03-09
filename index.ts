2faa5a0bb420eb4539a273fe3e7c3e22c7a8a128
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-03-09 23:35:45 +0530

    ts example

--- index.ts
+++ index.ts
@@ -293,6 +293,30 @@ var Car = (function () {
    return Car;
 }());
 
+class Car { 
+   //field 
+   engine:string; 
+   
+   //constructor 
+   constructor(engine:string) { 
+      this.engine = engine 
+   }  
+   
+   //function 
+   disp():void { 
+      console.log("Function displays Engine is  :   "+this.engine) 
+   } 
+} 
+
+//create an object 
+var obj = new Car("XXSY1")
+
+//access the field 
+console.log("Reading attribute value Engine as :  "+obj.engine)  
+
+//access the function
+obj.disp()
+
 
 
 
