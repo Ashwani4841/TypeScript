70a175a2a3db420e2086f694f27c8c66e163e71d
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-03-09 23:24:00 +0530

    ts example

--- index.ts
+++ index.ts
@@ -264,6 +264,21 @@ console.log(obj2);
 let obj = { name: "Sourav", age: 23 };
 console.log(Object.keys(obj).length);
 
+class Car { 
+   //field 
+   engine:string; 
+ 
+   //constructor 
+   constructor(engine:string) { 
+      this.engine = engine 
+   }  
+
+   //function 
+   disp():void { 
+      console.log("Engine is  :   "+this.engine) 
+   } 
+}
+
 
 
 
